8f7f596f1ae3132e1d31b3ec5e3001d2f053811a
Kyrylo <kirilllikholetov3@gmail.com>
2024-08-22 21:58:55 +0200

    added calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Layout from './components/Layout/Layout';
+import Calendar from './pages/Calendar/Calendar';
 import Dashboard from './pages/Dashboard/Dashboard';
 
 const App = () => {
@@ -7,7 +8,10 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout/>}>
+
           <Route path="dashboard" element={<Dashboard/>}/>
+          <Route path="calendar" element={<Calendar/>}/>
+
         </Route>
       </Routes>
     </BrowserRouter>
